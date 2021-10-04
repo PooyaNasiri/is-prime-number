5bd116b42b82f3f82b5b1f31b9adbcc0931432c2
Pooya Nasiri <pooya.nasiri75@gmail.com>
2021-10-04 20:27:26 +0330

    IsPrimeNumber

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace PrimeNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.85D;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int p; 
+     
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int q = 1, c = 0;
+            long a = -1;
+            char[] d;
+
+
+            d = textBox1.Text.ToCharArray();
+
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                q = 0;
+                for (int j = 48; j <= 57; j++)
+                    if (d[i] == j)
+                        q = 1;
+                if (q == 0)
+                {
+                    p = 0;
+                    goto a;
+                }
+                else
+                    p = 1;
+            }
+
+        a:
+            if (textBox1.Text == "")
+                label2.Text = " ";
+            else if (p == 1)
+                a = int.Parse(textBox1.Text);
+            else
+                a = -2;
+
+            for (int i = 1; i <= a; i++)
+                if (a % i == 0)
+                    c++;
+
+            if (a == -1)
+                label2.Text = "";
+            else if (a == -2)
+            {
+
+                label2.Text = "\" " + textBox1.Text + " \" is not a number";
+                label2.ForeColor = Color.Red; 
+            }
+            else if (a == 0 || a == 1)
+            {
+                label2.Text = a + " is not Prime or Non Prime";
+
+                label2.ForeColor = Color.HotPink; 
+            }
+            else if (c <= 2)
+            {
+                label2.Text = a + " is a Prime number. ";
+                label2.ForeColor = Color.Green; 
+            }
+            else
+            {
+                label2.Text = a + " is not a Prime number.";
+                label2.ForeColor = Color.OrangeRed; 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
